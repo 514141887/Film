12bc9fcc6b39e79ccdbf63d8a013f705fcbb5b5b
hecheng <hecheng@topcheer.com>
2018-09-15 22:21:57 +0800

    测试修改提交代码

---
+++ src/main/java/com/java1234/controller/admin/TestController.java
@@ -0,0 +1,8 @@
+package com.java1234.controller.admin;
+
+public class TestController {
+
+    public static void main(String[] args) {
+        System.out.println("this is my test demo");
+    }
+}
